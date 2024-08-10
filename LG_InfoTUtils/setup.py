12289d3f1dbd8a2933b8ded1b95b2830855f6f14
“Luciano <L.Gerber@mmu.ac.uk>
2024-08-10 21:53:19 +0100

    bootstraping package

--- LG_InfoTUtils/setup.py
+++ LG_InfoTUtils/setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='lg_mlutils',
+    name='LG_InfoTUtils',
     version='0.1',
     packages=find_packages(),
-    description='My humble generic ML utils, primarily for my own projects.',
+    description='My humble generic functionality for working with Information Theoretic metrics within numpy/sklearn/scipy, primarily for my own projects.',
     author='Luciano Gerber',
     author_email='L.Gerber@mmu.ac.uk',
     license='BSD-3'
